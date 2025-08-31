afd5c743841092b5537ec370efc2b35c3573d551
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-08-31 09:01:33 +0000

    refac: tests in folder

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:08:18 by codespace         #+#    #+#             */
-/*   Updated: 2025/08/31 08:28:33 by codespace        ###   ########.fr       */
+/*   Updated: 2025/08/31 09:00:49 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 int	main(void)
 {
 	// int	fd = 0;
-	int fd = open("normal_test.txt", O_RDONLY);
-	// int fd = open("only_nl_test.txt", O_RDONLY);
-	// int fd = open("1char_test.txt", O_RDONLY);
+	int fd = open("tests/normal_test.txt", O_RDONLY);
+	// int fd = open("tests/only_nl_test.txt", O_RDONLY);
+	// int fd = open("tests/1char_test.txt", O_RDONLY);
+	// int fd = open("tests/empty.txt", O_RDONLY);
 	if (fd == -1)
 		return (1);
 	int count = 0;
