e46d5be916114f15897580a5d8af4f28ea6140d1
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-08-31 06:23:37 +0000

    test: only nl

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:08:18 by codespace         #+#    #+#             */
-/*   Updated: 2025/08/31 05:06:00 by codespace        ###   ########.fr       */
+/*   Updated: 2025/08/31 06:13:01 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 int	main(void)
 {
-	int fd = open("test.txt", O_RDONLY);
+	// int	fd = 0;
+	// int fd = open("normal_test.txt", O_RDONLY);
+	int fd = open("only_nl_test.txt", O_RDONLY);
 	if (fd == -1)
 		return (1);
-	// int	fd = 0;
 	int count = 0;
 	char *line = get_next_line(fd);
 	while (line && count < 20)
