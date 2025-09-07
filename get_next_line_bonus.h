c1b56ceaa84cee957fd44c3d75de4ab79cfc0b64
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-09-07 18:51:36 +0900

    add bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msakurai <msakurai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 15:32:16 by codespace         #+#    #+#             */
+/*   Updated: 2025/09/07 16:31:59 by msakurai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unistd.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdbool.h>
+
+typedef struct s_list
+{
+	char			content[BUFFER_SIZE + 1];
+	struct s_list	*next;
+}				t_list;
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(char *s);
+void	ft_clear(t_list **list, char **content);
