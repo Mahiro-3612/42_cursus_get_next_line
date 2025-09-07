9369758fe3883c1e474fc394e415ba5be1fe5f47
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-09-07 17:46:45 +0900

    fit get next line in 25

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: msakurai <msakurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 15:31:42 by codespace         #+#    #+#             */
-/*   Updated: 2025/09/07 17:43:21 by msakurai         ###   ########.fr       */
+/*   Updated: 2025/09/07 17:46:05 by msakurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,11 +128,9 @@ char	*get_next_line(int fd)
 
 	list = NULL;
 	if (next_buf)
-	{
 		ft_lstadd_back_and_free(&list, &next_buf);
-		if (!list)
-			return (NULL);
-	}
+	if (next_buf && !list)
+		return (NULL);
 	bytes_read = 1;
 	while (!has_newline(list, bytes_read) && bytes_read > 0)
 	{
