81bc26558f92275e4b02492e6e4eb543149fc9b5
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-09-25 11:51:02 +0000

    add: test

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TEST_FILE="short.txt"
+
+for i in $(seq 1 100)
+do
+    echo "------------------------------------------"
+    echo "BUFFER_SIZE = $i でテスト中..."
+    echo "------------------------------------------"
+
+    rm -f a.out
+    cc -Wall -Wextra -Werror -D BUFFER_SIZE=$i get_next_line.c get_next_line_utils.c get_next_line.h main.c
+
+    if [ -f "a.out" ]; then
+        ./a.out "$TEST_FILE"
+    else
+        echo "コンパイルエラー: BUFFER_SIZE = $i"
+    fi
+done
+
+echo "全てのテストが完了しました。"
